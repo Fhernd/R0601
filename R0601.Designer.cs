a0092dd561efc9cf51cc0583dab7ad63b54c44b7
John Ortiz Ordoñez <johnortizo@outlook.com>
2015-11-25 08:42:07 -0500

    Creación formulario principal

---
+++ R0601.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace R0601
+{
+    partial class R0601
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblArchivo = new System.Windows.Forms.Label();
+            this.txtRutaXml = new System.Windows.Forms.TextBox();
+            this.btnCargarXml = new System.Windows.Forms.Button();
+            this.trvXml = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // lblArchivo
+            // 
+            this.lblArchivo.AutoSize = true;
+            this.lblArchivo.Location = new System.Drawing.Point(13, 13);
+            this.lblArchivo.Name = "lblArchivo";
+            this.lblArchivo.Size = new System.Drawing.Size(46, 13);
+            this.lblArchivo.TabIndex = 0;
+            this.lblArchivo.Text = "Archivo:";
+            // 
+            // txtRutaXml
+            // 
+            this.txtRutaXml.Location = new System.Drawing.Point(66, 13);
+            this.txtRutaXml.Name = "txtRutaXml";
+            this.txtRutaXml.Size = new System.Drawing.Size(412, 20);
+            this.txtRutaXml.TabIndex = 1;
+            // 
+            // btnCargarXml
+            // 
+            this.btnCargarXml.Location = new System.Drawing.Point(484, 12);
+            this.btnCargarXml.Name = "btnCargarXml";
+            this.btnCargarXml.Size = new System.Drawing.Size(75, 23);
+            this.btnCargarXml.TabIndex = 2;
+            this.btnCargarXml.Text = "Cargar";
+            this.btnCargarXml.UseVisualStyleBackColor = true;
+            this.btnCargarXml.Click += new System.EventHandler(this.btnXml_Click);
+            // 
+            // trvXml
+            // 
+            this.trvXml.Location = new System.Drawing.Point(16, 42);
+            this.trvXml.Name = "trvXml";
+            this.trvXml.Size = new System.Drawing.Size(543, 281);
+            this.trvXml.TabIndex = 3;
+            // 
+            // R0601
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 335);
+            this.Controls.Add(this.trvXml);
+            this.Controls.Add(this.btnCargarXml);
+            this.Controls.Add(this.txtRutaXml);
+            this.Controls.Add(this.lblArchivo);
+            this.Name = "R0601";
+            this.Text = "R0601 - XmlTree";
+            this.Load += new System.EventHandler(this.R0601_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblArchivo;
+        private System.Windows.Forms.TextBox txtRutaXml;
+        private System.Windows.Forms.Button btnCargarXml;
+        private System.Windows.Forms.TreeView trvXml;
+    }
+}
+
---
+++ R0601.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace R0601
+{
+    public partial class R0601 : Form
+    {
+        public R0601()
+        {
+            InitializeComponent();
+        }
+
+        private void R0601_Load(object sender, EventArgs e)
+        {
+            txtRutaXml.Text = Path.Combine(Application.StartupPath, @"CatalogoProductos.xml");
+        }
+		
+		private void btnXml_Click(object sender, System.EventArgs e)
+		{
+			// Limpia el componente TreeView:
+			trvXml.Nodes.Clear();
+			
+			// Car el documento XML:
+			XmlDocument docXml = new XmlDocument();
+			
+			try
+			{
+                docXml.Load(txtRutaXml.Text);
+			}
+			catch(Exception ex)
+			{
+                MessageBox.Show(this, "Problema al cargar el documento XML.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
+			
+			// Carga el contenido en el componente TreeView:
+			ConvertirXmlNodeATreeNode(docXml, trvXml.Nodes);
+			
+			// Expande todos los nodos:
+			trvXml.Nodes[0].ExpandAll();
+		}
+		
+		private void ConvertirXmlNodeATreeNode(XmlNode nodoXml, TreeNodeCollection nodos)
+		{
+			// Agregar un objeto TreeNode que represnte el nodo XmlNode actual:
+			TreeNode nuevoTreeNode = nodos.Add(nodoXml.Name);
+			
+			// Determina el elemento a mostrar:
+			switch(nodoXml.NodeType)
+			{
+				case XmlNodeType.ProcessingInstruction:
+				case XmlNodeType.XmlDeclaration:
+					nuevoTreeNode.Text = String.Format("<?{0} {1}?>", nodoXml.Name, nodoXml.Value) ;
+					break;
+				case XmlNodeType.Element:
+					nuevoTreeNode.Text = String.Format("<{0}>", nodoXml.Name);
+					break;
+				case XmlNodeType.Attribute:
+					nuevoTreeNode.Text = String.Format("ATTRIBUTE: {0}", nodoXml.Name);
+					break;
+				case XmlNodeType.Text:
+				case XmlNodeType.CDATA:
+					nuevoTreeNode.Text = String.Format("{0}", nodoXml.Value);
+					break;
+				case XmlNodeType.Comment:
+					nuevoTreeNode.Text = String.Format("<!--{0}-->", nodoXml.Value);
+					break;
+			}
+			
+			// Invocación recursiva por cada atributo del elemento:
+			if (nodoXml.Attributes != null)
+			{
+				foreach(XmlAttribute atributo in nodoXml.Attributes)
+				{
+					ConvertirXmlNodeATreeNode(atributo, nuevoTreeNode.Nodes);
+				}
+			}
+			
+			// Invocación recursiva para cada nodo hijo:
+			foreach(XmlNode nodoHijo in nodoXml.ChildNodes)
+			{
+				ConvertirXmlNodeATreeNode(nodoHijo, nuevoTreeNode.Nodes);
+			}
+		}
+    }
+}
